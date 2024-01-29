7abf2c0544fd282aa34a839b4e4e9acc53c19e09
sergei <osp11011983@mail.ru>
2024-01-29 21:59:54 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int initialAmount = 100;
+
+        int depositedAmount = 1005;
+
+        int bonus;
+        if (depositedAmount > 1005) {
+            bonus = depositedAmount/100;
+        } else {
+            bonus = 0;
+        }
+        bonus = depositedAmount / 100;
+        int summ = initialAmount + depositedAmount + bonus;
+        System.out.println("Баланс стал:" + summ);
+        System.out.println("Бонусы:" + bonus);
+
+    }
 }
